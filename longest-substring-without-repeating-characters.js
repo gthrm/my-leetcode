b85f6f4f4e35a31e45c83fa7231ba34f76eaa866
cdROma <mrv24ru@yandex.ru>
2020-12-06 02:21:49 +0300

    feat: longest-substring-without-repeating-characters added

---
+++ longest-substring-without-repeating-characters.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let map = {};
+  let start = 0;
+  let maxLen = 0;
+  let arr = s.split("");
+
+  for (i = 0; i < s.length; i++) {
+    let current = map[arr[i]];
+
+    if ((current !== null || current !== undefined) && start <= current) {
+      start = current + 1;
+    } else {
+      maxLen = Math.max(maxLen, i - start + 1);
+    }
+
+    map[arr[i]] = i;
+  }
+  return maxLen;
+};
+
+console.log(lengthOfLongestSubstring('pwwkew'));
