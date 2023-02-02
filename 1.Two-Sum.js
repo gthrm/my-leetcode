3e698c3714961b9e9b12622958f28d2a3d4d15dd
cdROma <mrv24ru@yandex.ru>
2023-02-02 17:25:42 +0100

    feat: 1 added

---
+++ 1.Two-Sum.js
@@ -0,0 +1,42 @@
+const nums1 = [2, 7, 11, 15],
+  target1 = 9;
+const nums2 = [3, 2, 4],
+  target2 = 6;
+const nums3 = [3, 3],
+  target3 = 6;
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// var twoSum = function (nums, target) {
+//   let i = 0;
+//   while (nums.length > 0) {
+//     const num = nums.shift();
+//     for (let index = 0; index < nums.length; index++) {
+//       const element = nums[index];
+//       if (num + element === target) {
+//         return [i, index + 1 + i];
+//       }
+//     }
+//     i++;
+//   }
+// };
+
+var twoSum = function (nums, target) {
+  if (nums.length === 2) {
+    return [0, 1];
+  }
+  const temp = {};
+  for (let index = 0; index < nums.length; index++) {
+    const element = nums[index];
+    if (temp.hasOwnProperty(target - element)) {
+      return [temp[target - element], index];
+    }
+    temp[element] = index;
+  }
+};
+
+console.log(twoSum(nums1, target1), "[0,1]");
+console.log(twoSum(nums2, target2), "[1,2]");
+console.log(twoSum(nums3, target3), "[0,1]");
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - [two-sum](https://github.com/gthrm/my-leetcode/blob/main/two-sum.js)
 - [add-two-numbers](https://github.com/gthrm/my-leetcode/blob/main/add-two-numbers.js)
 - [longest-substring-without-repeating-characters](https://github.com/gthrm/my-leetcode/blob/main/longest-substring-without-repeating-characters.js)
+- [1. Two Sum](https://github.com/gthrm/my-leetcode/blob/main/1.Two-Sum.js)
 - [9. Palindrome Number](https://github.com/gthrm/my-leetcode/blob/main/9.Palindrome-Number.js)
 - [14. Longest Common Prefix](https://github.com/gthrm/my-leetcode/blob/main/14.Longest-Common-Prefix.js)
 - [121. Best Time to Buy and Sell Stock](https://github.com/gthrm/my-leetcode/blob/main/121.Best-Time-to-Buy-and-Sell-Stock.js)
